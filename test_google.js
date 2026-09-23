c2be36c9153ba2c22eb8f665ed5854a4d7a99e67
rahuljploft331 <rahul_sharma@jploft.in>
2026-09-23 18:21:12 +0530

    chore: remove temporary patch scripts

--- test_google.js
+++
@@ -1,3 +0,0 @@
-import { google } from "googleapis";
-const p = google.androidpublisher({ version: "v3" });
-console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(p.purchases.subscriptions)));
